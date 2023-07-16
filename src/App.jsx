682c4060f05c95d1a940426fd80774a4ddf61ea7
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-07-16 16:43:57 +0600

    basic client site setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+    </>
+  );
+}
+
+export default App;
