953c8d9bde56bc21ca23fb05cf8a8031bfa013cf
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-07-17 13:48:18 +0600

    react helmet resolved

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Menu = () => {
   return (
     <div>
+      <Helmet>
+        <title>Bistro || Menu</title>
+      </Helmet>
       <h2>tis is menuuuuuuuuuuuuuuuuuuuu</h2>
     </div>
   );
