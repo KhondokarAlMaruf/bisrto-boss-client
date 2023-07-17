32d52b24cb4375c15bac858f8e28edd61322b39c
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-07-17 15:18:26 +0600

    parallax added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuBg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +9,12 @@ const Menu = () => {
       <Helmet>
         <title>Bistro || Menu</title>
       </Helmet>
-      <h2>tis is menuuuuuuuuuuuuuuuuuuuu</h2>
+      <Cover img={menuBg} title="ourr menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuBg} title="ourr menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuBg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
